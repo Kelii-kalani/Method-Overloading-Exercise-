77c19859c38377afb43a31f1841fe35a9ede3575
Michael <keliikalani87@gmail.com>
2023-02-08 20:48:21 -0800

    initial commit

---
+++ MethodOverloading/AddMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class AddMethods
+    {
+        public static int Add(int numero1, int numero2) 
+        {
+            return numero1 + numero2;
+        }
+        public static double Add(double numero1, double numero2) 
+        {           
+            return numero1 + numero2;
+        }
+        public static string Add(int numero1, int numero2, bool numero3)
+        {
+            int total = numero1 + numero2;
+
+            if (numero3 == true && total > 1)
+            {
+                return $"{total} dollars";
+            }
+            else if (numero3 == true && total == 1) 
+            {             
+                return $"{total} dollar";
+            }
+            else
+            {
+                return "Lose Money";
+            }
+        }
+    }
+}
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(AddMethods.Add(4, 7));
+            Console.WriteLine(AddMethods.Add(5.3, 8.6));
+            Console.WriteLine(AddMethods.Add(1, 5, true));
         }
     }
 }
